ba9f9621f950b9d0fa3557361bdb18c241086e4f
Vikram Bhatt <vikrambhatt4817@gmail.com>
2026-02-20 12:08:30 +0100

    Worked on creating class methods for the StoryMaker.

--- StoryMaker.py
+++ StoryMaker.py
@@ -1,25 +1,64 @@
 from dotenv import dotenv_values
 from openai import OpenAI
 
+config = dotenv_values(".env")
+open_ai_key = config["OPENROUTER_API"]
+
 class StoryMaker:
 
     url = "https://openrouter.ai/api/v1"
-    __config = dotenv_values(".env")
-    __open_ai_key = __config["OPENROUTER_API"]
+    basic_prompt = "Write a paragraph length story about a hero who goes on a journey, meets alies, gets stronger, finds weapons and treasure, and defeats the demon king."
+    main_model = "arcee-ai/trinity-large-preview:free"
+    fallback_models = ["deepseek/deepseek-r1-0528:free", "meta-llama/llama-3.3-70b-instruct:free"]
 
     def __init__(self):
-        self.client = OpenAI(base_url=self.url, api_key=self.__open_ai_key)
-        self.main_model = "arcee-ai/trinity-large-preview:free"
-        self.fallback_models = ["deepseek/deepseek-r1-0528:free", "meta-llama/llama-3.3-70b-instruct:free"]
-        self.basic_prompt = "Write a paragraph length story about a hero who goes on a journey, meets alies, gets stronger, finds weapons and treasure, and defeats the demon king."
-
-    def generate(self, prompt="", temp=1, stream_result=False, max_tokens=5000):
+        self.client = OpenAI(base_url=self.url, api_key=open_ai_key)
+        self.preserve_convo = {}
+        
+    def generate(self, prompt="", temp=1, stream_result=False, token_max=5000):
         if prompt == "":
             self.first_prompt = self.basic_prompt
         else:
             self.first_prompt = prompt
-
-        pass
+        
+        response = self.client.chat.completions.create(
+            model=self.main_model, 
+            messages=[
+                {   "role": "user", 
+                    "content": self.first_prompt
+                }
+            ],
+            extra_body={
+                "models": self.fallback_models,
+                "reasoning": {"effort": "low"}
+            },
+            max_tokens=token_max,
+            # stream=stream_result,
+            temperature=temp
+        )
 
     def update(self):
         pass
+
+    @classmethod
+    def create_story_prompt(cls):
+        pass
+
+    @classmethod
+    def get_api_url(cls):
+        return f"Base url: {cls.url}"
+    
+    @classmethod
+    def get_main_model(cls):
+        return f"Main model: {cls.main_model}"
+    
+    @classmethod
+    def get_basic_prompt(cls):
+        return f"If you do not specify a story prompt, you will get the following prompt:\n {cls.basic_prompt}"
+    
+    @classmethod
+    def get_fallback_model(cls):
+        fallback_str = "The following are fallback models in case the main model doesn't work:\n"
+        for index, model in enumerate(cls.fallback_models):
+            fallback_str += f"({index}) {model}, \n"
+        return fallback_str
--- main.py
+++ main.py
@@ -2,6 +2,10 @@ from StoryMaker import StoryMaker
 from FantasyHelper import FantasyHelper
 
 def main():
+    print(StoryMaker().get_api_url())
+    print(StoryMaker().get_basic_prompt())
+    print(StoryMaker().get_main_model())
+    print(StoryMaker().get_fallback_model())
     pass
 
 
